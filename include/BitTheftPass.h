bacb2baf2c3434eeadce2068390ecac3bf3a4434
Patrick Li <patrli@umich.edu>
2024-04-19 17:34:13 -0400

    feat: detection helper function

--- include/BitTheftPass.h
+++ include/BitTheftPass.h
@@ -1,13 +1,20 @@
 #ifndef LLVM_TRANSFORMS_SCALAR_BIT_THEFT_H
 #define LLVM_TRANSFORMS_SCALAR_BIT_THEFT_H
 
+#include <llvm/IR/Function.h>
 #include <llvm/IR/PassManager.h>
+#include <unordered_map>
+#include <vector>
 
 namespace llvm {
 
 class BitTheftPass : public PassInfoMixin<BitTheftPass> {
   public:
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+    std::vector<Argument *> getBitTheftCandidateI1(Function &F);
+    std::unordered_map<Argument *, uint64_t>
+    getBitTheftCandidatePtr(Function &F);
+    uint64_t getMinSpareBitsInPtr(Function &F, Argument *arg);
 };
 
 } // end namespace llvm
--- src/BitTheftPass.cpp
+++ src/BitTheftPass.cpp
@@ -1,8 +1,64 @@
 #include "BitTheftPass.h"
+#include <llvm/IR/Instructions.h>
+#include <llvm/Support/raw_ostream.h>
 
 #define DEBUG_TYPE "bit-theft"
 
 namespace llvm {
+
+std::vector<Argument *> BitTheftPass::getBitTheftCandidateI1(Function &F) {
+    std::vector<Argument *> candidates;
+    for (auto &arg : F.args()) {
+        if (arg.getType()->isIntegerTy(1)) {
+            candidates.push_back(&arg);
+        }
+    }
+    return candidates;
+}
+
+std::unordered_map<Argument *, uint64_t>
+BitTheftPass::getBitTheftCandidatePtr(Function &F) {
+    std::unordered_map<Argument *, uint64_t> candidates;
+    for (auto &arg : F.args()) {
+        if (arg.getType()->isPointerTy()) {
+            candidates[&arg] = getMinSpareBitsInPtr(F, &arg);
+        }
+    }
+    return candidates;
+}
+
+uint64_t BitTheftPass::getMinSpareBitsInPtr(Function &F, Argument *arg) {
+    size_t minAlignment = std::numeric_limits<size_t>::max();
+    for (auto &block : F) {
+        for (auto &inst : block) {
+            // If the instruction is a load instruction
+            // and the pointer operand is the argument
+            // we are interested in, we can check the
+            // alignment of the load instruction.
+            if (auto *loadInst = dyn_cast<LoadInst>(&inst)) {
+                if (loadInst->getPointerOperand() == arg) {
+                    Align alignment = loadInst->getAlign();
+                    if (alignment < minAlignment) {
+                        minAlignment = alignment.value();
+                    }
+                }
+            }
+
+            // If the instruction is a store instruction
+            // and the pointer operand is the argument
+            // Do the same
+            if (auto *storeInst = dyn_cast<StoreInst>(&inst)) {
+                if (storeInst->getPointerOperand() == arg) {
+                    Align alignment = storeInst->getAlign();
+                    if (alignment < minAlignment) {
+                        minAlignment = alignment.value();
+                    }
+                }
+            }
+        }
+    }
+    return Log2_64(minAlignment);
+}
 
 PreservedAnalyses BitTheftPass::run(Module &M, ModuleAnalysisManager &AM) {
     errs() << "Module Pass: " << M.getName() << '\n';
