b1bf2e7b62d33012d67978a444b96b2547743a4b
explocion <bhqin@umich.edu>
2024-04-25 15:49:56 -0400

    modified rand_walk

--- tests/rand_walk.c
+++ tests/rand_walk.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 int getCounter(int *counters, int x, int y, int N) {
     return counters[x * N + y];
@@ -11,11 +11,12 @@ void setCounter(int *counters, int x, int y, int N) { counters[x * N + y]++; }
 
 int result = 0;
 void walk(int *counters, int x, int y, int N, int depth, bool goVerticle,
-          bool goHorizontal, uint64_t a1, uint64_t a2, uint64_t a3, uint64_t a4, uint64_t a5, uint64_t a6) {
+          bool goHorizontal, uint64_t a1, uint64_t a2, uint64_t a3, uint64_t a4,
+          uint64_t a5, uint64_t a6) {
     x = x % N;
     y = y % N;
     setCounter(counters, x, y, N);
-    if (depth == 1000) {
+    if (depth == 128) {
         result += x + y;
         // Print out the entire counter array
         // printf("\n=====================\n");
@@ -33,23 +34,27 @@ void walk(int *counters, int x, int y, int N, int depth, bool goVerticle,
     // If go verticle, randomly choose to go up or down
     if (goVerticle) {
         rand() ? walk(counters, x - 1, y, N, depth + 1, nextGoVerticle,
-                      nextGoHorizontal, a1 + a2, a3 + a4, a5 + a6, a1 + a2, a3 + a4, a5 + a6)
+                      nextGoHorizontal, a1 + a2, a3 + a4, a5 + a6, a1 + a2,
+                      a3 + a4, a5 + a6)
                : walk(counters, x + 1, y, N, depth + 1, nextGoVerticle,
-                      nextGoHorizontal, a1 + a2, a3 + a4, a5 + a6, a1 + a2, a3 + a4, a5 + a6);
+                      nextGoHorizontal, a1 + a2, a3 + a4, a5 + a6, a1 + a2,
+                      a3 + a4, a5 + a6);
     }
     // if go horizontal, randomly choose to go left or right
     if (goHorizontal) {
         rand() ? walk(counters, x, y - 1, N, depth + 1, nextGoVerticle,
-                      nextGoHorizontal, a1 + a2, a3 + a4, a5 + a6, a1 + a2, a3 + a4, a5 + a6)
+                      nextGoHorizontal, a1 + a2, a3 + a4, a5 + a6, a1 + a2,
+                      a3 + a4, a5 + a6)
                : walk(counters, x, y + 1, N, depth + 1, nextGoVerticle,
-                      nextGoHorizontal, a1 + a2, a3 + a4, a5 + a6, a1 + a2, a3 + a4, a5 + a6);
+                      nextGoHorizontal, a1 + a2, a3 + a4, a5 + a6, a1 + a2,
+                      a3 + a4, a5 + a6);
     }
 }
 
 int main() {
     srand(10);
-    int N = 8;
+    const int N = 2;
     int *counters = (int *)malloc(N * N * sizeof(int));
     walk(counters, N / 2, N / 2, N, 0, 1, 1, 1, 2, 3, 4, 5, 6);
-    printf("%d", result);
+    printf("%d\n", result);
 }
